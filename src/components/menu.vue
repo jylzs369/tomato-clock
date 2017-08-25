ba7c5db57cfb1e2b830bfaf5bd24ff699d67fc35
Ushio <jylzs369@163.com>
2017-08-25 17:39:58 +0800

    添加菜单组件

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="menu flex">
+  <footer class="menu flex border-top">
     <router-link tag="a" :to="{ name: 'Task' }" class="tomato-icon icon-list"></router-link>
     <router-link tag="a" :to="{ name: 'Clock' }" class="tomato-icon icon-clock"></router-link>
-    <router-link tag="button" :to="{ name: 'Progress' }">进行</router-link>
+    <span>
+      <router-link tag="button" class="break tomato-icon primary" :class="'icon-start'" :to="{ name: 'Progress' }"></router-link>
+    </span>
     <router-link tag="a" :to="{ name: 'Statistics' }" class="tomato-icon icon-bar"></router-link>
     <router-link tag="a" to="" class="tomato-icon icon-user"></router-link>
-  </div>
+  </footer>
 </template>
